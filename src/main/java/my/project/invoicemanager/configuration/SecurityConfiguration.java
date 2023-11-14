bf1eebe5c8e496c39d76df8d6fc4084d237acef2
Magda <magdalenazur86@gmail.com>
2023-11-14 18:12:34 +0100

    Adding AuthenticationManager bean AccessDeniedHandler implementation and AuthenticationEntryPoint implementation

--- src/main/java/my/project/invoicemanager/configuration/SecurityConfiguration.java
+++ src/main/java/my/project/invoicemanager/configuration/SecurityConfiguration.java
@@ -1,15 +1,32 @@
 package my.project.invoicemanager.configuration;
 
+import lombok.RequiredArgsConstructor;
+import my.project.invoicemanager.handler.CustomAccessDeniedHandler;
+import my.project.invoicemanager.handler.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.OPTIONS;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration {
 
+    private final BCryptPasswordEncoder encoder;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
     private static final String[] PUBLIC_URLS = {};
 
     @Bean
@@ -17,11 +34,19 @@ public class SecurityConfiguration {
         http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(request -> request.requestMatchers(PUBLIC_URLS).permitAll());
-        http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.OPTIONS).permitAll());
-        http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.DELETE, "user/delete/**").hasAnyAuthority("DELETE:USER"));
-        http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.DELETE, "customer/delete/**").hasAnyAuthority("DELETE:CUSTOMER"));
-        http.exceptionHandling(exception -> exception.accessDeniedHandler(null).authenticationEntryPoint(null));
+        http.authorizeHttpRequests(request -> request.requestMatchers(OPTIONS).permitAll());
+        http.authorizeHttpRequests(request -> request.requestMatchers(DELETE, "user/delete/**").hasAnyAuthority("DELETE:USER"));
+        http.authorizeHttpRequests(request -> request.requestMatchers(DELETE, "customer/delete/**").hasAnyAuthority("DELETE:CUSTOMER"));
+        http.exceptionHandling(exception -> exception.accessDeniedHandler(customAccessDeniedHandler).authenticationEntryPoint(authenticationEntryPoint));
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(null);
+        authProvider.setPasswordEncoder(encoder);
+        return new ProviderManager(authProvider);
     }
 }
---
+++ src/main/java/my/project/invoicemanager/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package my.project.invoicemanager.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import my.project.invoicemanager.model.HttpResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static java.time.LocalTime.now;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        HttpResponse httpResponse = HttpResponse.builder()
+                .timeStamp(now().toString())
+                .reason("You don't have enough permission")
+                .status(FORBIDDEN)
+                .statusCode(FORBIDDEN.value())
+                .build();
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
